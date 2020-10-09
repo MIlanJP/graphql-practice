c092993b9f314c9ec2ed657681e321d07a3d5912
Milan Gowda <46890041+MIlanJP@users.noreply.github.com>
2020-10-09 09:24:25 +0530

    Added name and location in graphql

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
-import {GraphQLServer} from 'graphql-yoga';
+import { GraphQLServer } from "graphql-yoga";
 
 // Type defnitions (schema)
 
-const typeDefs=`
+const typeDefs = `
 type Query {
     hello: String!
+    name: String!
+    location: String!
 
 }
-`
+`;
 
 // Resolvers
 
-const resolvers={
-    Query:{
-        hello(){
-            return "This is My First Query!!"
-        }
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is My First Query!!";
+    },
+    name() {
+      return "My Name is Milan";
+    },
+    location(){
+        return "I Stay In Bangalore"
     }
-}
+  },
+};
 
-const server =new GraphQLServer({
-    typeDefs,
-    resolvers,
-})
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
 
-server.start(()=>{
-    console.log("The server is Up")
-})
+server.start(() => {
+  console.log("The server is Up");
+});
