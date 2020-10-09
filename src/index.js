92095a1aae019d7a64f1c54842879866995ddfcf
Milan Gowda <46890041+MIlanJP@users.noreply.github.com>
2020-10-09 09:21:35 +0530

    Added demo query and executed in playground

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello Graph QL")
+import {GraphQLServer} from 'graphql-yoga';
+
+// Type defnitions (schema)
+
+const typeDefs=`
+type Query {
+    hello: String!
+
+}
+`
+
+// Resolvers
+
+const resolvers={
+    Query:{
+        hello(){
+            return "This is My First Query!!"
+        }
+    }
+}
+
+const server =new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+
+server.start(()=>{
+    console.log("The server is Up")
+})
