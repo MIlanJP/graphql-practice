dd58cb75ba4e8bd9231de7aef9fa901f4dfab4a4
Milan Gowda <46890041+MIlanJP@users.noreply.github.com>
2020-10-09 12:38:49 +0530

    Added examples for array and custom Object arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import { GraphQLServer } from "graphql-yoga";
 
+const users=[{
+    id:"100",
+    name:"Milan",
+    email:"milan@example.com",
+    age:25
+},
+{
+    id:"101",
+    name:"Rakesh",
+    email:"Rakesh@example.com",
+    age:27
+},{
+    id:"102",
+    name:"Michal",
+    email:"Michal@example.com",
+    age:28
+}
+]
+
 // Type defnitions (schema)
 
 const typeDefs = `
 type Query {
 me: User!
 film: Movie!
+users:[User!]
 posts:Posts!
+listofBooks:[String!]!
 greeting(name:String,location:String): String!
 add(firstNumber:Int!,secondNumber:Int!):Int!
 subtract(firstNumber:Int!,secondNumber:Int!):Int!
@@ -30,48 +51,54 @@ type User{
     age: Int
 }
 
+
 `;
 
 // Resolvers
 
 const resolvers = {
   Query: {
-    me(){
-        return{
-            id:"123",
-            name:"Milan",
-            email:"milan@gmail.com",
-            age:25
-
-        }
+      users(){
+          return users
+      },
+    me() {
+      return {
+        id: "123",
+        name: "Milan",
+        email: "milan@gmail.com",
+        age: 25,
+      };
     },
-    film(){
-        return {
-            title: "Don",
-            releaseYear: 1980,
-            budget:50
-        }
+    film() {
+      return {
+        title: "Don",
+        releaseYear: 1980,
+        budget: 50,
+      };
     },
-    posts(){
-        return {
-            title:'My First Post',
-            comments:"Hey Nice Post",
-            published:true
-        }
+    posts() {
+      return {
+        title: "My First Post",
+        comments: "Hey Nice Post",
+        published: true,
+      };
     },
-    greeting(parent,args,ctx,info){
-        if(args.name&& args.location){
-            return "Hello "+ args.name+ " you stay in " + args.location
-        }else{
-            return "Hello"
-        }
-        },
-        add(parent,args,ctx,info){
-            return args.firstNumber+args.secondNumber
-        },
-        subtract(parent,args,ctx,info){
-            return args.firstNumber-args.secondNumber
-        },
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.location) {
+        return "Hello " + args.name + " you stay in " + args.location;
+      } else {
+        return "Hello";
+      }
+    },
+    add(parent, args, ctx, info) {
+      return args.firstNumber + args.secondNumber;
+    },
+    subtract(parent, args, ctx, info) {
+      return args.firstNumber - args.secondNumber;
+    },
+    listofBooks() {
+        return ["Art of Living","Online Writer","The Dark Knight"]
+    },
   },
 };
 
